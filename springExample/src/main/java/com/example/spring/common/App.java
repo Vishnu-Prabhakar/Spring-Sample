98afcb02f940fb374a5b95bfc27a5ce280814044
Ragupathy <Vishnu.Ragupathy@nielsen.com>
2019-05-24 19:35:59 +0530

    Boiler plate code for basic spring project

---
+++ springExample/src/main/java/com/example/spring/common/App.java
@@ -0,0 +1,18 @@
+package com.example.spring.common;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml"); 
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+    }
+}
