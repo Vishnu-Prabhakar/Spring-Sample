4bdf5483ee6ddc38a853b70ad5f6a73f77f982a6
Ragupathy <vishnuprabhakarr@gmail.com>
2020-12-05 22:53:38 +0530

    Added comment for swagger config

--- springbootmysql/src/main/java/com/example/springbootmysql/SwaggerConfig.java
+++ springbootmysql/src/main/java/com/example/springbootmysql/SwaggerConfig.java
@@ -9,6 +9,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * 
+ * Swagger Config for the application
+ */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {  
