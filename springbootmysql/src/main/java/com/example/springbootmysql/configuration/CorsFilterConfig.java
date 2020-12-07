27e777c029d464ddb3f98735f41bcddeea9c1b1a
Ragupathy <vishnuprabhakarr@gmail.com>
2020-12-07 23:19:27 +0530

    Cors Filter comment

--- springbootmysql/src/main/java/com/example/springbootmysql/configuration/CorsFilterConfig.java
+++ springbootmysql/src/main/java/com/example/springbootmysql/configuration/CorsFilterConfig.java
@@ -7,6 +7,10 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+/**
+ * 
+ * Cors Filter
+ */
 @Configuration
 public class CorsFilterConfig {
 
